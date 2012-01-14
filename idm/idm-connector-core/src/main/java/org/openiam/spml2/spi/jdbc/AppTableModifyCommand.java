a88ab7a4a01347734e7049ef779a8bb1a26872bd
Suneet Shah <suneet_shah@openiam.com>
2012-01-14 17:09:18 -0500

    apptable connector updated so that a missing identity is reprovisioned during a modify operation

--- idm/idm-connector-core/src/main/java/org/openiam/spml2/spi/jdbc/AppTableModifyCommand.java
+++ idm/idm-connector-core/src/main/java/org/openiam/spml2/spi/jdbc/AppTableModifyCommand.java
@@ -1,22 +1,18 @@
 package org.openiam.spml2.spi.jdbc;
 
 import org.openiam.idm.srvc.mngsys.dto.ManagedSys;
-import org.openiam.idm.srvc.pswd.service.PasswordGenerator;
 import org.openiam.idm.srvc.res.dto.Resource;
-import org.openiam.idm.srvc.res.dto.ResourceProp;
-import org.openiam.provision.type.*;
-import org.openiam.spml2.msg.*;
-//import org.openiam.spml2.msg.ExtensibleAttribute;
 import org.openiam.provision.type.ExtensibleAttribute;
-import org.openiam.spml2.msg.ModificationType;
+import org.openiam.provision.type.ExtensibleObject;
+import org.openiam.spml2.msg.*;
 
-
-import javax.naming.NamingException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.text.ParseException;
 import java.util.List;
+
+//import org.openiam.spml2.msg.ExtensibleAttribute;
 
 /**
  * Implements modify capability for the AppTableConnector
@@ -27,12 +23,12 @@ import java.util.List;
  */
 public class AppTableModifyCommand extends AppTableAbstractCommand {
 
-     public ModifyResponseType modify(ModifyRequestType reqType) {
-          String tableName;
+    public ModifyResponseType modify(ModifyRequestType reqType) {
+        String tableName;
         String principalName = null;
-           Connection con = null;
-                     String principalFieldName = null;
-            String principalFieldDataType = null;
+        Connection con = null;
+        String principalFieldName = null;
+        String principalFieldDataType = null;
 
         ModifyResponseType response = new ModifyResponseType();
         response.setStatus(StatusCodeType.SUCCESS);
@@ -41,21 +37,21 @@ public class AppTableModifyCommand extends AppTableAbstractCommand {
 
 
         String requestID = reqType.getRequestID();
-          /* PSO - Provisioning Service Object -
-             *     -  ID must uniquely specify an object on the target or in the target's namespace
-             *     -  Try to make the PSO ID immutable so that there is consistency across changes. */
+        /* PSO - Provisioning Service Object -
+*     -  ID must uniquely specify an object on the target or in the target's namespace
+*     -  Try to make the PSO ID immutable so that there is consistency across changes. */
         PSOIdentifierType psoID = reqType.getPsoID();
-        principalName  = psoID.getID();
+        principalName = psoID.getID();
 
-          /* targetID -  */
+        /* targetID -  */
         String targetID = psoID.getTargetID();
-          /* ContainerID - May specify the container in which this object should be created
-             *      ie. ou=Development, org=Example */
-         PSOIdentifierType containerID = psoID.getContainerID();
+        /* ContainerID - May specify the container in which this object should be created
+*      ie. ou=Development, org=Example */
+        PSOIdentifierType containerID = psoID.getContainerID();
 
 
-         /* A) Use the targetID to look up the connection information under managed systems */
-          ManagedSys managedSys = managedSysService.getManagedSys(targetID);
+        /* A) Use the targetID to look up the connection information under managed systems */
+        ManagedSys managedSys = managedSysService.getManagedSys(targetID);
 
         if (managedSys.getResourceId() == null || managedSys.getResourceId().length() == 0) {
             response.setStatus(StatusCodeType.FAILURE);
@@ -73,75 +69,90 @@ public class AppTableModifyCommand extends AppTableAbstractCommand {
         }
 
         // modificationType contains a collection of objects for each type of operation
-         List<ModificationType> modificationList = reqType.getModification();
+        List<ModificationType> modificationList = reqType.getModification();
 
-         log.debug("ModificationList = " + modificationList);
-         log.debug("Modificationlist size= " + modificationList.size());
+        log.debug("ModificationList = " + modificationList);
+        log.debug("Modificationlist size= " + modificationList.size());
 
 
-         List<ModificationType> modTypeList = reqType.getModification();
+        List<ModificationType> modTypeList = reqType.getModification();
 
-         try {
-             con = connectionMgr.connect(managedSys);
+        try {
+            con = connectionMgr.connect(managedSys);
 
             // build sql
-            StringBuffer whereBuf = new StringBuffer(" WHERE ");
-            StringBuffer updateBuf = new StringBuffer("UPDATE " + tableName);
+            StringBuilder whereBuf = new StringBuilder(" WHERE ");
+            StringBuilder updateBuf = new StringBuilder("UPDATE " + tableName);
             updateBuf.append(" SET ");
 
-        for (ModificationType mod : modTypeList) {
-            ExtensibleType extType = mod.getData();
-            List<ExtensibleObject> extobjectList = extType.getAny();
+            for (ModificationType mod : modTypeList) {
+                ExtensibleType extType = mod.getData();
+                List<ExtensibleObject> extobjectList = extType.getAny();
 
-            int ctr = 0;
-
-
-            for (ExtensibleObject obj : extobjectList) {
-                    List<ExtensibleAttribute> attrList = obj.getAttributes();
-                    principalFieldName =  obj.getPrincipalFieldName();
-                    principalFieldDataType = obj.getPrincipalFieldDataType();
-
-                    whereBuf.append( principalFieldName + " = ? " );
-
-                    for (ExtensibleAttribute att : attrList) {
+                int ctr = 0;
 
 
-                        if (att.getOperation() != 0 && att.getName() != null) {
-                            if ( att.getObjectType().equalsIgnoreCase("USER"))  {
-                                if (ctr != 0) {
-                                    updateBuf.append(",");
+                for (ExtensibleObject obj : extobjectList) {
+
+                    if (identityExists(con, tableName, principalName, obj)) {
+
+                        log.debug("Identity found. Modifying identity: " + principalName);
+
+                        List<ExtensibleAttribute> attrList = obj.getAttributes();
+                        principalFieldName = obj.getPrincipalFieldName();
+                        principalFieldDataType = obj.getPrincipalFieldDataType();
+
+                        whereBuf.append(principalFieldName + " = ? ");
+
+                        for (ExtensibleAttribute att : attrList) {
+
+
+                            if (att.getOperation() != 0 && att.getName() != null) {
+                                if (att.getObjectType().equalsIgnoreCase("USER")) {
+                                    if (ctr != 0) {
+                                        updateBuf.append(",");
+                                    }
+                                    ctr++;
+
+                                    updateBuf.append(att.getName() + " = ? ");
                                 }
-                                ctr++;
-
-                                updateBuf.append( att.getName() + " = ? ");
                             }
                         }
+
+                        updateBuf.append(whereBuf);
+                        log.debug(" SQL=" + updateBuf.toString());
+
+                        PreparedStatement statement = con.prepareStatement(updateBuf.toString());
+
+                        ctr = 1;
+                        for (ExtensibleAttribute att : attrList) {
+                            
+                            if (att.getOperation() != 0 && att.getName() != null) {
+
+                                if (att.getObjectType().equalsIgnoreCase("USER")) {
+                                    setStatement(statement, ctr, att);
+                                    ctr++;
+                                }
+
+                            }
+
+                        }
+                        if (principalFieldName != null) {
+
+                            setStatement(statement, ctr, principalFieldDataType, principalName);
+                        }
+
+
+                        statement.executeUpdate();
+                    } else {
+
+                        // identity does not exist in the target system
+                        // identity needs to be re-provisioned
+                        addIdentity(con, tableName, principalName, obj);
                     }
 
-                updateBuf.append( whereBuf );
-                log.debug(" SQL=" + updateBuf.toString());
-
-                PreparedStatement statement = con.prepareStatement(updateBuf.toString());
-
-                ctr = 1;
-                 for (ExtensibleAttribute att : attrList) {
-                     if (att.getOperation() != 0 && att.getName() != null) {
-                            if ( att.getObjectType().equalsIgnoreCase("USER"))  {
-                                setStatement(statement, ctr, att);
-                                ctr++;
-                            }
-                        }
-
-                 }
-                if (principalFieldName != null) {
-
-                    setStatement(statement, ctr, principalFieldDataType, principalName);
                 }
-
-                statement.executeUpdate();
-
             }
-        }
         } catch (SQLException se) {
             log.error(se.toString());
             response.setStatus(StatusCodeType.FAILURE);
@@ -171,27 +182,93 @@ public class AppTableModifyCommand extends AppTableAbstractCommand {
                     response.setStatus(StatusCodeType.FAILURE);
                     response.setError(ErrorCode.SQL_ERROR);
                     response.addErrorMessage(s.toString());
-                    return response;
                 }
             }
         }
 
 
-
         return response;
     }
 
+    private void addIdentity(Connection con, String tableName, String principalName, ExtensibleObject obj)
+            throws SQLException, ParseException {
+        // build sql
+        StringBuilder insertBuf = new StringBuilder("INSERT INTO " + tableName);
+        StringBuilder columnBuf = new StringBuilder("(");
+        StringBuilder valueBuf = new StringBuilder(" values (");
+
+        List<ExtensibleAttribute> attrList = obj.getAttributes();
+
+        String principalFieldName = obj.getPrincipalFieldName();
+        String principalFieldDataType = obj.getPrincipalFieldDataType();
+
+        log.debug("Adding identity: " + principalName);
+        log.debug("Number of attributes to persist in ADD = " + attrList.size());
+
+        int ctr = 0;
+        for (ExtensibleAttribute att : attrList) {
+            if (ctr != 0) {
+                columnBuf.append(",");
+                valueBuf.append(",");
+            }
+            ctr++;
+            columnBuf.append(att.getName());
+            valueBuf.append("?");
+        }
+        // add the primary key
+        log.debug("Principal column name=" + obj.getPrincipalFieldName());
+        if (obj.getPrincipalFieldName() != null) {
+            if (ctr != 0) {
+                columnBuf.append(",");
+                valueBuf.append(",");
+            }
+            columnBuf.append(obj.getPrincipalFieldName());
+            valueBuf.append("?");
+        }
+
+
+        columnBuf.append(")");
+        valueBuf.append(")");
+        //}
+        insertBuf.append(columnBuf);
+        insertBuf.append(valueBuf);
+
+        log.debug("ADD SQL=" + insertBuf.toString());
+
+        PreparedStatement statement = con.prepareStatement(insertBuf.toString());
+        // set the parameters
+
+
+        ctr = 1;
+        for (ExtensibleAttribute att : attrList) {
+
+            setStatement(statement, ctr, att);
+            ctr++;
+            log.debug("Binding parameter: " + att.getName() + " -> " + att.getValue());
+
+        }
+
+
+        if (obj.getPrincipalFieldName() != null) {
+
+            setStatement(statement, ctr, obj.getPrincipalFieldDataType(), principalName);
+        }
+
+
+        statement.executeUpdate();
+
+
+    }
 
 
     protected ModifyResponseType populateResp(ModifyResponseType response, StatusCodeType status,
-                                                      ErrorCode err, String msg) {
+                                              ErrorCode err, String msg) {
 
-                response.setStatus(status);
-                response.setError(err);
-                response.addErrorMessage(msg);
-                return response;
-            }
-
+        response.setStatus(status);
+        response.setError(err);
+        response.addErrorMessage(msg);
+        return response;
+    }
 
 
 }
