83a33d9d0a1840552d9b22796dacf600a851d963
Suneet Shah <suneet_shah@openiam.com>
2012-04-25 16:06:22 -0400

    added bat file for windows users

---
+++ lib/install-dependencies.bat
@@ -0,0 +1,7 @@
+
+mvn install:install-file -DgroupId=com.google.gdata -DartifactId=gdata-appsforyourdomain-1.0 -Dversion=1.41.5 -Dpackaging=jar -Dfile=gdata-appsforyourdomain-1.0-1.41.5.jar
+
+
+
+
+mvn install:install-file -DgroupId=ch.ethz.ssh2 -DartifactId=ganymed-ssh2 -Dversion=251beta1 -Dpackaging=jar -Dfile=ganymed-ssh2-build251beta1.jar
